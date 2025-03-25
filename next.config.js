7d778dcff907cbb6425a98c45586e9903b710840
llSiddharthll <siddharthgreat443@gmail.com>
2025-03-25 11:30:20 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,14 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack"],
-      });
-      return config;
-    },
-  };
-  
-export default nextConfig;
-  
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "brightlightimmigration.ca",
+          },
+        ],
+        destination: "https://www.brightlightimmigration.ca/:path*",
+        permanent: true, // 301 redirect
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
