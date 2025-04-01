aaea5e8d5d841c40e72232083b98e268212e34a0
llSiddharthll <siddharthgreat443@gmail.com>
2025-04-01 17:29:49 +0530

    updated

--- next-sitemap.cjs
+++
@@ -1,5 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://www.brightlightimmigration.ca/", // Change this
-  generateRobotsTxt: true,
-};
---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+export default {
+  siteUrl: "https://www.brightlightimmigration.ca/", // Change this if needed
+  generateRobotsTxt: true,
+  exclude: ['/blog/*'],
+  debug: true,
+};
