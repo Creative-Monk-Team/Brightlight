19267dc37cabd2a73f261b71c5a109f2265219b6
llSiddharthll <siddharthgreat443@gmail.com>
2025-05-13 13:21:03 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -21,6 +21,36 @@ const nextConfig = {
         destination: "https://www.brightlightimmigration.ca/:path*",
         permanent: true, // 301 redirect
       },
+      {
+        source: '/humanitarian-compassionate',
+        destination: '/humanitarian-compassionate',
+        permanent: true, // Good for SEO (301 Redirect)
+      },
+      {
+        source: '/canadian-experience-class',
+        destination: '/canadian-experience-class',
+        permanent: true, // Good for SEO (301 Redirect)
+      },
+      {
+        source: '/canadian-experience-class',
+        destination: '/canadian-experience-class',
+        permanent: true, // Good for SEO (301 Redirect)
+      },
+      {
+        source: '/pr-renewal',
+        destination: '/pr-renewal',
+        permanent: true, // Good for SEO (301 Redirect)
+      },
+      {
+        source: '/category-based',
+        destination: '/category-based',
+        permanent: true, // Good for SEO (301 Redirect)
+      },
+      {
+        source: '/blogs/top-10-reasons-for-refusal-on-your-canada-visitor-visa-application-in-2024-comprehensive-guide',
+        destination: '/blogs/top-10-reasons-for-refusal-on-your-canada-visitor-visa-application-in-2024-comprehensive-guide',
+        permanent: true, // Good for SEO (301 Redirect)
+      },
     ];
   },
 };
