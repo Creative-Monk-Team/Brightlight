230076d49934b0e11d3afae8699206037132ba94
llSiddharthll <siddharthgreat443@gmail.com>
2025-05-14 15:11:06 +0530

    update

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,24 +1,4 @@
-
-// import Navbar1 from "../components/Navbar1";
-// import Footer1 from "../components/Footer1";
-// let Custom404 = () => {
-//   return (
-//     <>
-//       <Navbar1 showBlue={true} />
-//         <div className={"h-[80vh] mt-32 flex justify-center items-center"}>
-//           <img
-//             className={"h-[40rem]"}
-//             src={"/assets/pageNotFoundImg.svg"}
-//             alt="Page not Found"
-//           />
-//         </div>
-//       <Footer1 />
-//     </>
-//   );
-// };
-
-// export default Custom404;
-
+ 
 
 // pages/404.js
 import { useEffect } from 'react';
