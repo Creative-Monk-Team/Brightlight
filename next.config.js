bab74a96b05b49b29c16ebd78d92c51cf9bebc70
llSiddharthll <siddharthgreat443@gmail.com>
2025-05-15 10:49:58 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   webpack(config) {
@@ -12,45 +11,11 @@ const nextConfig = {
     return [
       {
         source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "brightlightimmigration.ca",
-          },
-        ],
+        has: [{ type: "host", value: "brightlightimmigration.ca" }],
         destination: "https://www.brightlightimmigration.ca/:path*",
-        permanent: true, // 301 redirect
-      },
-      {
-        source: '/humanitarian-compassionate',
-        destination: '/humanitarian-compassionate',
-        permanent: true, // Good for SEO (301 Redirect)
-      },
-      {
-        source: '/canadian-experience-class',
-        destination: '/canadian-experience-class',
-        permanent: true, // Good for SEO (301 Redirect)
-      },
-      {
-        source: '/canadian-experience-class',
-        destination: '/canadian-experience-class',
-        permanent: true, // Good for SEO (301 Redirect)
-      },
-      {
-        source: '/pr-renewal',
-        destination: '/pr-renewal',
-        permanent: true, // Good for SEO (301 Redirect)
-      },
-      {
-        source: '/category-based',
-        destination: '/category-based',
-        permanent: true, // Good for SEO (301 Redirect)
-      },
-      {
-        source: '/blogs/top-10-reasons-for-refusal-on-your-canada-visitor-visa-application-in-2024-comprehensive-guide',
-        destination: '/blogs/top-10-reasons-for-refusal-on-your-canada-visitor-visa-application-in-2024-comprehensive-guide',
-        permanent: true, // Good for SEO (301 Redirect)
-      },
+        permanent: true,
+      }
+      // Remove all self-referencing redirects
     ];
   },
 };
