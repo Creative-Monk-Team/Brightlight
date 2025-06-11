c0ee1f133c261b4780528975b425397e942d7796
llSiddharthll <siddharthgreat443@gmail.com>
2025-06-11 13:20:42 +0530

    update

--- next.config.js
+++ next.config.js
@@ -40,6 +40,56 @@ const nextConfig = {
         destination: "https://www.brightlightimmigration.ca/",
         permanent: false, // Temporary redirect (302)
       },
+      {
+        source: "/ways-to-prove-you-will-leave-canada-at-the-end-of-your-temporary-stay",
+        destination: "https://www.brightlightimmigration.ca/blogs/8-ways-to-prove-you-will-leave-canada-at-the-end-of-your-temporary-stay-visitor-visa-study-permit-and-work-permit",
+        permanent: true,
+      },
+      {
+        source: "/about",
+        destination: "https://www.brightlightimmigration.ca/about-us",
+        permanent: true,
+      },
+      {
+        source: "/6-options-after-your-pgwp-expires",
+        destination: "https://www.brightlightimmigration.ca/blogs/6-options-after-your-pgwp-expires-staying-in-canada",
+        permanent: true,
+      },
+      {
+        source: "/outside-cananda",
+        destination: "https://www.brightlightimmigration.ca/outside-canada",
+        permanent: true,
+      },
+      {
+        source: "/business-investor-immigration",
+        destination: "https://www.brightlightimmigration.ca/business-visitor-visa",
+        permanent: true,
+      },
+      {
+        source: "/study-in-canada",
+        destination: "https://www.brightlightimmigration.ca/inside-canada",
+        permanent: true,
+      },
+      {
+        source: "/ways-to-prove-you-will-leave-canada-at-the-end-of-your-temporary-stay",
+        destination: "https://www.brightlightimmigration.ca/blogs/8-ways-to-prove-you-will-leave-canada-at-the-end-of-your-temporary-stay-visitor-visa-study-permit-and-work-permit",
+        permanent: true,
+      },
+      {
+        source: "/business-investor-immigration",
+        destination: "https://www.brightlightimmigration.ca/business-visitor-visa",
+        permanent: true,
+      },
+      {
+        source: "/agricultureand-agri-food-occu",
+        destination: "https://www.brightlightimmigration.ca/agriculture-agri-food-occupation",
+        permanent: true,
+      },
+      {
+        source: "/contact",
+        destination: "https://www.brightlightimmigration.ca/contact-us",
+        permanent: true,
+      },
     ];
   },
 };
